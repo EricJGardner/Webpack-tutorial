9c45b1414af567865116002171780fb053399701
Eric J Gardner <74524075+EricJGardner@users.noreply.github.com>
2020-12-31 15:50:14 -0600

    first commit from VSC
    necessary files minus the node modules to save space

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { render } from "react-dom";
+import { getUsers } from "./common/usersAPI";
+import "./style.scss";
+console.log("Hello webpack!");
+
+getUsers().then(json => console.log(json));
+
+function App() {
+    const [state, setState] = useState("CLICK ME");
+    return <button onClick={() =>setState("CLICKED")}>{state}</button>;
+}
+
+const fancyFunc = () => {
+    return [1, 2];
+};
+
+const [a, b] = fancyFunc();
+
+render(<App />, document.getElementById("root"));
